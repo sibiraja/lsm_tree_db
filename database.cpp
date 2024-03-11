1cf389fbee1c8caf1c430283ad6d50383d185a6d
SIBI RAJA <sraja@college.harvard.edu>
2024-03-11 02:41:25 -0400

    connected with parser, manual correctness tests are good, need to expand on real lsm tree implementation

---
+++ database.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "lsm.hh"
+
+using namespace std;
+
+void load(string& fileName, lsm_tree* lsm_tree_obj) {
+    ifstream file;
+    string newfileName = "generator/" + fileName;
+    file.open(newfileName, ios::binary);
+    if (file) {
+        while (file.good() && !file.eof()) {
+            int key;
+            int value;
+            file.read(reinterpret_cast<char*>(&key), sizeof(key));
+            file.read(reinterpret_cast<char*>(&value), sizeof(value));
+            // cout << "File: " << newfileName << " wants to insert (" << key << ", " << value << ")" << endl;
+            lsm_tree_obj->insert({key, value});
+            // put(key, value);
+        }
+        file.close();
+    } else {
+        cout << "File `" << newfileName << "` not found!" << endl;
+    }
+}
+
+void printStats() {
+    cout << "printStats() not implemented yet!" << endl;
+}
+
+int main() {
+    lsm_tree* db = new lsm_tree();
+    
+    string line;
+    while (getline(cin, line)) {
+        istringstream iss(line);
+        char command;
+        int key, value, startKey, endKey;
+        string fileName;
+        iss >> command;
+        switch (command) {
+            case 'p':
+                iss >> key >> value;
+                db->insert({key, value});
+                // put(key, value);
+                break;
+            case 'g':
+                iss >> key;
+                db->get(key);
+                // get(key);
+                break;
+            case 'd':
+                iss >> key;
+                db->delete_key(key);
+                // del(key);
+                break;
+            case 'r': {
+                int startKey, endKey;
+                iss >> startKey >> endKey;
+                db->range(startKey, endKey);
+                // range(startKey, endKey);
+                break;
+            }
+            case 'l': {
+                iss >> fileName;
+                load(fileName, db);
+                break;
+            }
+            case 's':
+                // TODO: need to implement printStats() function in `lsm.hh` once bloom filters and fence pointers are integrated
+                // printStats();
+                break;
+            default:
+                cout << "Unknown command: " << command << endl;
+        }
+    }
+    return 0;
+}
